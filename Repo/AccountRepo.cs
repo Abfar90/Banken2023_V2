6d8b5e002baed1c0a446bab835ca56a7c8f3f339
Abdi Farah <abdirahman.farah@live.se>
2023-03-07 13:30:15 +0100

    Added functions for withdraw, transfer and deposit

--- Repo/AccountRepo.cs
+++ Repo/AccountRepo.cs
@@ -1,13 +1,40 @@
-﻿using System;
+﻿using Banken2023_V2.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Banken2023_V2.Repo;
 
 namespace Banken2023_V2.Repo
 {
     internal class AccountRepo
     {
+        DataAccess access = new DataAccess();
+        public void transfer (string type, int id, int amount)
+        {
+            if (type == "Savings")
+            {
+                access.TransfertoCheckings(id, amount);
+            }
+
+            else if (type == "Checking")
+            {
+                access.TransfertoSavings(id, amount);
+            }
+
+        }
+
+        //deposit och withdraw nedan kan bli samma funktion
+        public void withdraw (int id, int amount)
+        {
+            access.withdraw(id, amount);
+        }
+
+        public void deposit (int id, int amount)
+        {
+            access.deposit(id, amount);
+        }
 
     }
 }
--- Repo/DataAccess.cs
+++ Repo/DataAccess.cs
@@ -10,7 +10,7 @@ using Banken2023_V2.Models;
 
 namespace Banken2023_V2.Repo
 {
-    internal class DataAccess
+    public class DataAccess
     {
         private BankDbContext _connection;
 
@@ -28,6 +28,70 @@ namespace Banken2023_V2.Repo
         public List<SavingAccount> GetSaving (int id)
         {
             return _connection.SavingAccounts.Where(x => x.UserId == id).ToList();
+        }
+
+        public decimal [] TransfertoSavings (int id, decimal amount)
+        {
+            CheckingAccount fromAccount = _connection.CheckingAccounts.SingleOrDefault(c => c.UserId == id);
+
+            decimal newCheckingBalance = fromAccount.BalanceChecking - amount;
+
+            _connection.Entry(fromAccount.BalanceChecking).CurrentValues.SetValues(newCheckingBalance);
+
+            SavingAccount toAccount = _connection.SavingAccounts.SingleOrDefault(c => c.UserId == id);
+
+            decimal newSavingsBalance = toAccount.BalanceSavings - amount;
+
+            _connection.Entry(toAccount.BalanceSavings).CurrentValues.SetValues(newSavingsBalance);
+
+            decimal[] newbalances = { newCheckingBalance, newSavingsBalance };
+
+            return newbalances;
+
+        }
+
+        public decimal [] TransfertoCheckings(int id, decimal amount)
+        {
+            SavingAccount fromAccount = _connection.SavingAccounts.SingleOrDefault(c => c.UserId == id);
+
+            decimal newCheckingBalance = fromAccount.BalanceSavings - amount;
+
+            _connection.Entry(fromAccount.BalanceSavings).CurrentValues.SetValues(newCheckingBalance);
+
+            CheckingAccount toAccount = _connection.CheckingAccounts.SingleOrDefault(c => c.UserId == id);
+
+            decimal newSavingsBalance = fromAccount.BalanceSavings - amount;
+
+            _connection.Entry(fromAccount.BalanceSavings).CurrentValues.SetValues(newSavingsBalance);
+
+            decimal[] newbalances = { newCheckingBalance, newSavingsBalance };
+
+            return newbalances;
+
+        }
+
+        public decimal withdraw(int id, decimal amount)
+        {
+            CheckingAccount account = _connection.CheckingAccounts.SingleOrDefault(c => c.UserId == id);
+
+            //lägg till logik som stoppar overdraft
+
+            decimal newCheckingBalance = account.BalanceChecking - amount;
+
+            _connection.Entry(account.BalanceChecking).CurrentValues.SetValues(newCheckingBalance);
+
+            return newCheckingBalance;
+        }
+
+        public decimal deposit(int id, decimal amount)
+        {
+            CheckingAccount account = _connection.CheckingAccounts.SingleOrDefault(c => c.UserId == id);
+
+            decimal newCheckingBalance = account.BalanceChecking - amount;
+
+            _connection.Entry(account.BalanceChecking).CurrentValues.SetValues(newCheckingBalance);
+
+            return newCheckingBalance;
         }
     }
 }
